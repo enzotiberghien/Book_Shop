23377bfbc41b22dc636be7e9ad1953a5a10a4353
enzotiberghien <enzotiberghien28@gmail.com>
2023-04-04 14:46:03 +0200

    json changes 2

--- pages/booksPage.js
+++ pages/booksPage.js
@@ -17,7 +17,7 @@ const setFilteredBooks = (value) => filteredBooks = value
 
 
 const start = async () => {
-  books = await getJSON('../utils/books.json');
+  books = await getJSON('../books.json');
   displayBooks(await books);
   addFilters()
   addFilterByAuthors()
